9e25cc1d7057d5c9e944a2a6a59324c25a2b3fb7
omkar2816 <omkarkorgaonkar376@gmail.com>
2024-04-24 11:34:56 +0530

    workspace management system

--- starter.py
+++ starter.py
@@ -50,18 +50,29 @@ class Stater(customtkinter.CTk):
         self.label = customtkinter.CTkLabel(self, image=self.image, text="")
         self.label.pack()
 
-        self.button = customtkinter.CTkButton(self, text="Get Started", width=140, height=28, corner_radius=16,
+        self.button = customtkinter.CTkButton(self, text="Get Started as Admin", width=140, height=28, corner_radius=16,
                                               bg_color="#491669", fg_color="#491669", text_color="#ffffff",
                                               font=("Century Gothic", 18, "underline"), hover=False,
-                                              command=self.login)
-        self.button.pack(anchor="center")
+                                              command=self.login_as_admin)
+        self.button.pack(anchor="center", side="left", padx=(175, 0))
 
-    def login(self):
+        self.button1 = customtkinter.CTkButton(self, text="Get Started as User", width=140, height=28, corner_radius=16,
+                                              bg_color="#491669", fg_color="#491669", text_color="#ffffff",
+                                              font=("Century Gothic", 18, "underline"), hover=False,
+                                              command=self.login_as_user)
+        self.button1.pack(anchor="ne", side="right", padx=(0, 175))
+
+    def login_as_user(self):
         self.destroy()
         import user_login
-        login = user_login.Login()
-        login.mainloop()
+        user_login = user_login.Login()
+        user_login.mainloop()
 
+    def login_as_admin(self):
+        self.destroy()
+        import admin_login
+        admin_login = admin_login.Starter()
+        admin_login.mainloop()
 
 if __name__ == '__main__':
     stater = Stater()
